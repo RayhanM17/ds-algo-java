66cb69db930827a245e4743f928b853957ca389a
RayhanM17 <marrero.rayhan@gmail.com>
2024-09-02 01:01:30 -0400

    LinkedList

--- src/main/java/com/rayhanm17/App.java
+++ src/main/java/com/rayhanm17/App.java
@@ -8,6 +8,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        LinkedList myLL = new LinkedList(1);
+
+        myLL.append(2);
+        myLL.append(3);
+        myLL.append(4);
+        myLL.append(5);
+        myLL.append(6);
+
+        myLL.printList();
+
+        System.out.println("Reversed List");
+
+        myLL.reverse();
+        
+        myLL.printList();
     }
 }
